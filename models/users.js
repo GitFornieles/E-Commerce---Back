eb40446507d5060f322cca67d809841b6708444a
GitFornieles <alefornieles@hotmail.com>
2022-10-31 18:25:48 -0300

    Schema Design

---
+++ models/users.js
@@ -0,0 +1,63 @@
+const S = require("sequelize");
+const db = require("../db");
+const bc = require("bcrypt");
+
+class Users extends S.Model {
+  //LLAMADA EN EL HOOK
+  createHash(string, salt) {
+    //Esta función crea el hash que se almacena en el campo "password" del usuario en la base de datos. 
+    //Parámetros: password ingresado por usuario y "salt" que se genera de forma aleatoria.
+    return bc.hash(string, salt);
+  }
+  validatePassword(password) {
+    //función para validar el password ingresado en el login
+    return bc.hash(password, this.salt).then((hash) => hash === this.password);
+  }
+}
+
+Users.init(
+  {
+    nickname: {
+        type:S.STRING,
+        allowNull:false,
+    },
+    email: {
+      type: S.STRING,
+      allowNull: false,
+    },
+    name:{
+        type: S.STRING,
+        allowNull: false,
+      },
+    lastname:{
+        type: S.STRING,
+        allowNull: false,
+      },
+    age:{
+        type: S.INTEGER,
+        allowNull: false,
+      },
+      address:{},
+      
+    password: {
+      type: S.STRING,
+      allowNull: false,
+    },
+    salt: {
+      type: S.STRING,
+    },
+  },
+  { sequelize: db, modelName: "user" }
+);
+
+Users.addHook("beforeCreate", (User) => {
+  User.salt = bc.genSaltSync();
+  return User
+    .createHash(User.password, User.salt)
+    .then((result) => {
+      User.password = result;
+    })
+    .catch((err) => console.log(err));
+});
+
+module.exports = Users;
